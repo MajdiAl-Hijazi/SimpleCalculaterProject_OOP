3c109028d68d6127a2e1ecb4f6524014c95b8a1a
Majdi AlHijazi <majdialhijazi@gmail.com>
2026-06-04 12:51:27 +0300

    Implement cancel last operation and fix typos
    Added functionality to cancel the last operation and fixed method names.

--- SimpleCalculaterProject_OOP.cpp
+++ SimpleCalculaterProject_OOP.cpp
@@ -3,38 +3,58 @@ using namespace std;
 
 class clsCalculator {
 
+private:
+
 	string _LastOperation = "clear";
 	float _LastNumber = 0;
 	float _Result = 0;
+	float _PreviousResult = 0;
+
+	//private methods...
+	bool _IsZero(float Number) {
+	
+		return (Number == 0);
+	}
 
 public:
+
 	void Clear() {
 
+		_PreviousResult = _Result;
 		_Result = 0;
 		_LastNumber = 0;
 		_LastOperation = "Clear";
 	}
+
 	void Add(float Num) {
 
+		_PreviousResult = _Result;
 		_Result += Num;
 		_LastNumber = Num;
 		_LastOperation = "Addaing";
 	}
+
 	void Subtract(float Num) {
 
+		_PreviousResult = _Result;
 		_Result -= Num;
 		_LastNumber = Num;
 		_LastOperation = "Subtracting";
 	}
+
 	void Multiplay(float Num) {
 
+		_PreviousResult = _Result;
 		_Result *= Num;
 		_LastNumber = Num;
 		_LastOperation = "Multiplaying";
 	}
+
 	void Divide(float Num) {
 
-		if (Num != 0) {
+		_PreviousResult = _Result;
+
+		if (!_IsZero(Num)) {
 
 			_Result /= Num;
 			_LastNumber = Num;
@@ -44,9 +64,17 @@ public:
 
 		_LastOperation = "Dividing";
 	}
+
 	void PrintResult() {
 
 		cout << "Result After " << _LastOperation << " " << _LastNumber << " is: " << _Result << endl;
+	}
+
+	void CancelLastOperation() {
+	
+		_LastNumber = 0;
+		_Result = _PreviousResult;
+		_LastOperation = "Cancelling Last Operation";
 	}
 };
 
@@ -63,6 +91,24 @@ int main() {
 	Calculator1.Subtract(120);
 	Calculator1.PrintResult();
 	Calculator1.Clear();
+	Calculator1.PrintResult();
+	Calculator1.Add(12.5);
+	Calculator1.PrintResult();
+	Calculator1.CancelLastOperation();
+	Calculator1.PrintResult();
+	Calculator1.Multiplay(6);
+	Calculator1.PrintResult();
+	Calculator1.Divide(12.5);
+	Calculator1.PrintResult();
+	Calculator1.Subtract(12.5);
+	Calculator1.PrintResult();
+	Calculator1.Divide(12.5);
+	Calculator1.PrintResult();
+	Calculator1.Add(0.52);
+	Calculator1.PrintResult();
+	Calculator1.Divide(0);
+	Calculator1.PrintResult();
+	Calculator1.CancelLastOperation();
 	Calculator1.PrintResult();
 
 	return 0;
